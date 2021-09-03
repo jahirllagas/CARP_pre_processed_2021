b7a88b212c03742e0ebc51f57a24f7051643c7a1
Rafael Martinelli <martinelli@puc-rio.br>
2021-09-03 11:52:08 -0300

    wip: Refactoring, new stubs

---
+++ src/LocalSearch.jl
@@ -0,0 +1,21 @@
+
+const SWAP_INTRA = 1
+const RELOCATE_INTRA = 2
+# ...
+
+function localSearch()
+
+    moved = true
+    while moved
+        moved = false
+
+        # moved = localSearchSwap()
+        # ...
+
+    end
+
+end
+
+function perturb()
+
+end
